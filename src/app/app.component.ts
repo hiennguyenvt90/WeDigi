daf0d767cab800bfb8046bc371a96e4c80f627a0
Kim Nguyen <nguyenxuongkim2103@gmail.com>
2019-05-23 10:09:50 +0300

    Add existing file

---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private oneSignal: OneSignal
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      this.oneSignal.startInit('dcda4330-f2ad-42a2-9742-ead8469cb359', '703243764093');
+
+      this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+
+      this.oneSignal.handleNotificationReceived().subscribe((res) => {
+        console.log(res);
+      });
+
+      this.oneSignal.handleNotificationOpened().subscribe((res) => {
+        // do something when a notification is opened
+        console.log(res);
+      });
+
+      this.oneSignal.endInit();
+    });
+  }
+}
